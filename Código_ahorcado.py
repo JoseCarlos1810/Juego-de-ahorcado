2bfdb28a36031a68c67c55d21de76f17b9faee1b
JoseCarlos1810 <A01708776@tec.mx>
2024-10-10 17:53:19 -0600

    Update Código_ahorcado.py

--- Código_ahorcado.py
+++ Código_ahorcado.py
@@ -57,7 +57,7 @@ dibujo= [
        |
 =========
 ''','''
-   _____
+  ______
    |   |
    O   |
   /|\  |
@@ -118,15 +118,30 @@ def pruebas():
         final()
         resultado(intentos)
 
+#Esta es una función donde se puede agregar palabras al final del archivo
+def agregar_palabras():
+    archivo=open("palabras.txt","a+")
+    agregar=input("Dime que palabra quieres que agregue: ")
+    archivo.write(agregar)
+    archivo.write("\n")
+    archivo.seek(0)
+    archivo.close()
+    
 #Esta es una función donde se elegira una palabra random de una lista de varias palabras
 def palabra_random():
-    lista_palabras=['Arándano','Frambuesa','Zarzamora','Mandarina','Naranja','Pomelo','Melón','Sandía','Aguacate','Carambola','Chirimoya','Coco','Dátil','Kiwi','Litchi','Papaya','Plátano','Cereza','Ciruela','Manzana','Melocotón','Nectarina','Níspero']
-    palabra=random.choice(lista_palabras)
+    archivo=open("palabras.txt","r")
+    archivo.seek(0)
+    lineas_archivo=archivo.readlines()
+    numero_lineas=len(lineas_archivo)
+    palabra_random=random.randint(0,numero_lineas)
+    palabra=lineas_archivo[palabra_random]
     return palabra
 
 #Esta es una función donde se imprimen las instrucciones de como jugar y hace que la palabra a adivinar sea ahorcado
 def tutorial():
-    print("En este juego tienes que intentar adivinar una palabra poniendo letras las cuales llenaran los espacios si están en la palabra, tienes un total de 6 intentos y te aparecerán las letras que ya intentaste, pero no estaban en la palabra, lo siguiente es una prueba donde la palabra es ahorcado, también hay que mencionar que no hay acentos ni símbolos en las palabras")
+    print("En este juego tienes que intentar adivinar una palabra poniendo letras las cuales llenaran los espacios si están en la palabra, tienes un total de \
+6 intentos y te aparecerán las letras que ya intentaste, pero no estaban en la palabra, lo siguiente es una prueba donde la palabra es ahorcado, también hay \
+que mencionar que no hay acentos ni símbolos en las palabras")
     palabra="ahorcado"
     return palabra
 
@@ -139,7 +154,9 @@ def tu_ponla():
 def corregir_palabra(palabra):
     palabra_minusculas=palabra.lower()
     palabra_ahorcado=normalize('NFKD', palabra_minusculas).encode('ASCII', 'ignore').decode('ASCII')
-    return palabra_ahorcado
+    ultima=len(palabra)
+    palabra_final=palabra_ahorcado[:-1]
+    return palabra_final
 
 #Esta es una función donde se hace una lista de espacios en blanco para saber la longitud de la palabra
 def espacio_palabras(letras):
@@ -159,14 +176,14 @@ def menu1(intentos):
     print("Incorrectas: ",errores)
 
 #Esta es una función donde se hará el proceso del ahorcado para verificar si esta bien o mal la letra que adivino el usuario, asi como llamar a las demás funciones
-def ahorcado(p):
+def ahorcado(op):
     intentos=0
     print("Solo hay letras del abecedario, en caso de poner un número o símbolo, se contará como incorrecto")
-    if p==1:
+    if op==1:
         palabra=palabra_random()
-    elif p==2:
+    elif op==2:
         palabra=tu_ponla()
-    elif p==3:
+    elif op==3:
         palabra=tutorial()
     palabra_ahorcado=corregir_palabra(palabra)
     letras=list(palabra_ahorcado)
@@ -194,14 +211,14 @@ def ahorcado(p):
         print("La palabra era:", palabra_ahorcado)
         final()
         resultado(intentos)
-        
 #Esta es una función donde se despliega el menu de opciones para que el usuario vea que puede hacer    
 def menu2():
     print("1. Solitario")
     print("2. Tu pon la palabra")
     print("3. Tutorial")
-    print("4. Salir")
-    print("5. Prueba")
+    print("4. Agregar palabra")
+    print("5. Salir")
+    print("6. Prueba")
     
 #Esta es una función donde se le pregunta al usuario que opción del menu quiere elegir y llamar a la función ahorcado de la forma que eliga el usuario
 def main():
@@ -209,7 +226,7 @@ def main():
     while continuar:
         menu2()
         op=input("Qué quieres hacer: ")
-        if op!="1" and op!="2" and op!="3" and op!="4" and op!="5":
+        if op!="1" and op!="2" and op!="3" and op!="4" and op!="5" and op!="6":
             print("Lo que pusiste es incorrecto")
         else:
             if op=="1":
@@ -219,8 +236,10 @@ def main():
             elif op=="3":
                 ahorcado(3)
             elif op=="4":
-                continuar=False
+                agregar_palabras()
             elif op=="5":
+                continuar=False
+            elif op=="6":
                 pruebas()
                 
 main()
